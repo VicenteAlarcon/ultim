a4c3632d8c1bb0163744488412ca3e529e968946
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-19 05:10:47 +0100

    feat:"vista index"

--- ultim/resources/views/users/index.blade.php
+++ ultim/resources/views/users/index.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+@extends('layouts.layout')
+@section('contenido')
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Email</th>
+      <th scope="col">Tipo</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($users as $user)
+    <tr>
+      <th scope="row">{{$user->id}}</th>
+      <td>{{$user->name}}</td>
+      <td>{{$user->email}}</td>
+      @if($user->type == null)
+      <td>Sin tipo</td>
+      @else
+      <td>{{$user->type}}</td>
+      @endif
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endsection
