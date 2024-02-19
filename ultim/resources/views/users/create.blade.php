0084d3020fc6b7f3feaf4960929ff7bef249df6f
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-19 05:33:29 +0100

    feat:"form register"

--- ultim/resources/views/users/create.blade.php
+++ ultim/resources/views/users/create.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+@extends('layouts.layout')
+@section('contenido')
+<form action="{{route('users.store')}}" method="post">
+    @csrf
+<div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" name="name">
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Type</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" name="type">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+@endsection
