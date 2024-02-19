e285d90a30f69ab4673e94187e5642dcfd68711c
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-19 04:36:20 +0100

    feat:"Servicios"

--- ultim/app/Http/Controllers/AuthController.php
+++ ultim/app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
---
+++ ultim/app/Services/UserControllerService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+use App\Models\User;
+
+class UserControllerService
+{
+
+   public function getAllUsers()
+   {
+    return User::all();
+   }
+
+   public function getConcreteUser($id)
+   {
+    return User::find($id);
+   }
+
+   public function createUser($userData)
+   {
+     return User::create($userData);
+
+   }
+
+   public function updateUser($userId, $userData)
+   {
+    $user = findOrFail($userId)
+    $user->update($userData);
+    return $user;
+
+   }
+
+}
